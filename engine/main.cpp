d4e795b85f54ba08c8357d2ed4ccb6d2b25bc5b9
l2ho.lV4l2u <naruesornprabpon@gmail.com>
2024-12-24 15:41:41 +0700

    delete Stockfish

--- engine/main.cpp
+++ engine/main.cpp
@@ -3,38 +3,28 @@
 #include <cstdlib> 
 #include <ctime>   
 #include "engine.h"
-#include "chess.cpp"
-#include "movegen.cpp"
+#include "board.cpp"
+#include "move.cpp"
 
 using namespace std;
 
 int main() {
+    bool turn = 0; // 0 for white, 1 for black
+    string initialBoard = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w"; // Initial board state using FEN
     string input;
-    /*
     srand(static_cast<unsigned int>(time(nullptr)));
     while (input != "q") {
         getline(cin, input);
-        Chess chess(input);
-        vector <string> moves = findAllMoves(chess);
+        if (input == "q") {
+            break;
+        }
+        Board board(input);
+        vector<string> moves = findMoves(board);
         if (!moves.empty()) {
             // Pick a random index from the moves vector
             int randomIndex = rand() % moves.size();
-            cout << moves[randomIndex] << endl;
-        } 
-        else {
-            cout << "No valid moves available." << endl;
-            break;
+            cout << moves[randomIndex] << endl;     
         }
     }
-    */
-    getline(cin, input);
-    Chess chess(input);
-    vector <string> moves = findAllMoves(chess);
-    Chess newchess = chess.makeMove(moves[0]);
-    chess.showBoard();
-    cout << "-------------------" << endl;
-    cout << moves[0] << endl;
-    cout << "-------------------" << endl;
-    newchess.showBoard();
     return 0;
 }
